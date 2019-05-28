c049c9667a96c1c004697bcc92f09d966b267923
Dino Angelo Reyes <d1no_ang3lo@hotmail.com>
2019-05-28 08:13:51 +0800

    changed world to dino

--- Practice2/Program.cs
+++ Practice2/Program.cs
@@ -6,7 +6,7 @@ namespace Practice2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Dino!");
         }
     }
 }
